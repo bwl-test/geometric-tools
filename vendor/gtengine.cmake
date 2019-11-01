e31a882a11bb505e243672982a8968670fb1ff6a
白文磊 <baiwenlei@sogou-inc.com>
2019-11-01 18:40:12 +0800

    GTEngine改为子模块形式管理

--- vendor/gtengine.cmake
+++ vendor/gtengine.cmake
@@ -1,24 +1,15 @@
-ExternalProject_Add(
-    gtengine-download
-    URL https://www.geometrictools.com/Downloads/GeometricToolsEngine3p28.zip
-    SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}
-    CONFIGURE_COMMAND ""
-    BUILD_COMMAND ""
-    INSTALL_COMMAND ""
-)
-
-ADD_CUSTOM_TARGET( 
-    gtengine-symlink 
-    ALL  
+set(GTENGINE_INCLUDE_DIR  ${CMAKE_CURRENT_LIST_DIR}/GTEngine/include)
+set(GTENGINE_INCLUDE_LINK ${CMAKE_CURRENT_LIST_DIR}/GTEngine/GTEngine)
+add_custom_target( 
+    gtengine-symlink
     COMMAND ${CMAKE_COMMAND} -E create_symlink
-    ${CMAKE_CURRENT_LIST_DIR}/GeometricTools/GTEngine/include
-    ${CMAKE_CURRENT_LIST_DIR}/GeometricTools/GTEngine/GTEngine
+    ${GTENGINE_INCLUDE_DIR}
+    ${GTENGINE_INCLUDE_LINK}
 )
-add_dependencies(gtengine-symlink gtengine-download)
 
 add_library(gtengine INTERFACE)
 target_include_directories(
     gtengine
-    INTERFACE vendor/GeometricTools/GTEngine
+    INTERFACE ${GTENGINE_INCLUDE_LINK}
 )
 add_dependencies(gtengine gtengine-symlink)
--- vendor/vendor.cmake
+++ vendor/vendor.cmake
@@ -1,2 +1,7 @@
+execute_process(
+    COMMAND git submodule update --init
+    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/vendor
+)
+
 include(${CMAKE_CURRENT_LIST_DIR}/catch2.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/gtengine.cmake)
